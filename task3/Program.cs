b94ca15ee4b6daed8ecd4c9f574390a9363034f8
nikolays <sidorovkolya91@gmail.com>
2023-01-27 16:12:10 +0300

    Задача3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,3 +1,26 @@
-﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Сделать через функции
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// Сделать через функции
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+int[] CreatArray(int size)
+{
+  int[] massive = new int[size];
+  Random rnd = new Random();
+  for (int i = 0; i < massive.Length; i++)
+  {
+    massive[i] = rnd.Next(0, 2);
+  }
+  return massive;
+}
+void PrintArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    System.Console.Write(array[i] + ",");
+  }
+}
+
+
+int[] array = CreatArray(8);
+PrintArray(array);
